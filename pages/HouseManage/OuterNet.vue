dfec27386d2ab0fe01d523db28a87078a4cb2ee7
CrazyPhilip <cf20090901@outlook.com>
2020-11-13 18:22:43 +0800

    新增多项功能

--- pages/HouseManage/OuterNet.vue
+++ pages/HouseManage/OuterNet.vue
@@ -3,7 +3,21 @@
 		<u-navbar is-back="true" title="全网房源"></u-navbar>
 
 		<view>
-			<u-row gutter="16" justify="center">
+			<u-dropdown ref="uDropdown" activeColor="#2979ff">
+				<!-- <u-dropdown-item @change="change1" v-model="value1" :options="areaList"></u-dropdown-item> -->
+				<u-dropdown-item :title="areaList[value1].label">
+					<view class="slot-content" style="background-color: #FFFFFF;">
+						<scroll-view scroll-y="true" style="height: 500rpx;">
+							<view class="u-text-center u-content-color u-m-t-30 u-m-b-30" v-for="(item, index) in areaList" @click="change1(item.value)">{{item.label}}</view>
+						</scroll-view>
+					</view>
+				</u-dropdown-item>
+				<u-dropdown-item @change="change2" v-model="value2" :title="fangxingList[value2].label" :options="fangxingList"></u-dropdown-item>
+				<u-dropdown-item @change="change3" v-model="value3" :title="priceList[value3].label" :options="priceList"></u-dropdown-item>
+				<u-dropdown-item @change="change4" v-model="value4" :title="squareList[value4].label" :options="squareList"></u-dropdown-item>
+			</u-dropdown>
+
+			<!-- <u-row gutter="16" justify="center">
 				<u-col span="3">
 					<view>
 
@@ -18,7 +32,7 @@
 						<u-dropdown-item @change="change4" v-model="value4" :title="options4[value4].label" :options="options4"></u-dropdown-item>
 					</u-dropdown>
 				</u-col>
-			</u-row>
+			</u-row> -->
 		</view>
 
 		<view class="uni-list">
@@ -45,43 +59,12 @@
 						</u-row>
 					</view>
 				</view>
-				
-				<!-- <u-row @tap="ToHouseDetail(index)">
-					<u-col span="4" @tap="ToHouseDetail(index)">
-						<u-image width="200" height="200" border-radius="8" :src="item.proCoverUrl" error-icon="/static/icon/NullPic.png"
-						 mode="aspectFill"></u-image>
-					</u-col>
-
-					<u-col span="8" @tap="ToHouseDetail(index)">
-						<view style="margin-left: 8rpx;">
-							<view>
-								<text class="BiKan">必看</text>
-								<text class="HouseTitle">{{item.proTitle}}</text>
-							</view>
-							<view>
-								<text class="HouseTag">{{item.proDistrict}}</text>
-								<text class="HouseTag">{{item.proArea}}</text>
-								<text class="HouseTag">VR</text>
-								<text class="HouseTag">随时看房</text>
-							</view>
-							<view class="">{{item.proCountF}}室{{item.proCountT}}厅/{{item.proSquare}}㎡/{{item.proDirection}}/{{item.EstateName}}</view>
-							<view>
-								<u-row>
-									<u-col class="HousePrice" span="6">{{item.proPrice}}{{item.proPriceType}}</u-col>
-									<u-col class="HouseUnitPrice" span="6">{{item.proUnitPrice}}{{item.proUnitPriceType}}</u-col>
-								</u-row>
-							</view>
-						</view>
-					</u-col>
-				</u-row> -->
 			
 			</view>
 		</view>
 
 		<u-loadmore :status="status" :icon-type="iconType" :load-text="loadText" />
 		<u-back-top :scroll-top="scrollTop"></u-back-top>
-		<u-select v-model="show" :default-value="defaultValue" mode="mutil-column-auto" :list="list" @confirm="confirm"
-		 @cancel="cancel"></u-select>
 	</view>
 </template>
 
@@ -89,114 +72,148 @@
 	export default {
 		data() {
 			return {
+				searchParam: {
+					areaId: 0,
+					areaLevel: 0,
+					completeYearRanges: [{
+						maxValue: 100,
+						minValue: 0
+					}],
+					countFRanges: [{
+						maxValue: 100,
+						minValue: 0
+					}],
+					decorations: [],
+					directions: [],
+					floors: [],
+					houseAreaRanges: [{
+						maxValue: 100,
+						minValue: 0
+					}],
+					houseTypes: [],
+					keyWords: "",
+					rentRanges: [{
+						maxValue: 100,
+						minValue: 0
+					}],
+					saleRanges: [{
+						maxValue: 100,
+						minValue: 0
+					}],
+					sortByHouseArea: -1,
+					sortByLatest: -1,
+					sortBySoldPrice: -1,
+					sortByUnitPrice: -1
+				},
 				scrollTop: 0,
 				current: 0,
-				result: '区域',
 				show: false,
+				value1: 0,
 				value2: 0,
 				value3: 0,
 				value4: 0,
 				defaultValue: [0, 0, 0],
-				options2: [{
+				areaList: [{
+					value: 0,
+					label: '全部区域'
+				}],
+
+				fangxingList: [{
 						label: '全部房型',
 						value: 0,
 					},
 					{
-						label: '1房',
+						label: '1室',
 						value: 1,
 					},
 					{
-						label: '2房',
+						label: '2室',
 						value: 2,
 					},
 					{
-						label: '3房',
+						label: '3室',
 						value: 3,
 					},
 					{
-						label: '4房及以上',
+						label: '4室',
 						value: 4,
+					},
+					{
+						label: '5室',
+						value: 5,
+					},
+					{
+						label: '5室及以上',
+						value: 6,
 					},
 				],
 
-				options3: [{
+				priceList: [{
 						label: '全部价格',
 						value: 0,
 					},
 					{
-						label: '0-30万',
+						label: '小于40万',
 						value: 1,
 					},
 					{
-						label: '30-50万',
+						label: '50-80万',
 						value: 2,
 					},
 					{
-						label: '50-100万',
+						label: '80-100万',
 						value: 3,
 					},
 					{
-						label: '100-150万',
+						label: '100-120万',
 						value: 4,
 					},
 					{
-						label: '150-200万',
+						label: '120-200万',
 						value: 5,
 					},
 					{
-						label: '200-300万',
+						label: '200-500万',
 						value: 6,
 					},
 					{
-						label: '300-500万',
+						label: '大于500万',
 						value: 7,
-					},
-					{
-						label: '500万及以上',
-						value: 8,
 					},
 				],
 
-				options4: [{
+				squareList: [{
 						label: '全部面积',
 						value: 0,
 					},
 					{
-						label: '0-20平米',
+						label: '小于50㎡',
 						value: 1,
 					},
 					{
-						label: '20-50平米',
+						label: '50-70㎡',
 						value: 2,
 					},
 					{
-						label: '50-100平米',
+						label: '70-90㎡',
 						value: 3,
 					},
 					{
-						label: '100-150平米',
+						label: '90-120㎡',
 						value: 4,
 					},
 					{
-						label: '150-200平米',
+						label: '120-140㎡',
 						value: 5,
 					},
 					{
-						label: '200-250平米',
+						label: '140-200㎡',
 						value: 6,
 					},
 					{
-						label: '250-500平米',
+						label: '大于200㎡',
 						value: 7,
-					},
-					{
-						label: '500-800平米',
-						value: 8,
-					},
-					{
-						label: '800平米以上',
-						value: 9,
-					},
+					}
 				],
 
 				list: [{
@@ -245,8 +262,8 @@
 				],
 
 				houseList: [],
-				
-				houseCount:0,
+
+				houseCount: 0,
 
 				pageSize: 20,
 				pageNum: 1,
@@ -261,12 +278,13 @@
 		},
 
 		onLoad() {
+			this.getAreaList();
 			this.GetHouseList();
 		},
-		
+
 		onReachBottom() {
 			this.status = 'loading';
-			this.pageNum = ++ this.pageNum;
+			this.pageNum = ++this.pageNum;
 			this.GetHouseList();
 		},
 
@@ -293,6 +311,10 @@
 				console.log(e);
 			},
 
+			change1(e) {
+				this.value1 = e;
+				this.$refs.uDropdown.close();
+			},
 			change2(e) {
 				this.value2 = e;
 			},
@@ -304,28 +326,110 @@
 			},
 
 			GetHouseList() {
-				this.$u.post('http://47.108.202.57:8090/property/search?cityPinYin=chengdu&pageNum=' + this.pageNum + '&pageSize=' +
-					this.pageSize, {
+				uni.request({
+					url: 'https://gl.junjunhouse.com/property/search?cityPinYin=chengdu&pageNum=' + this.pageNum +
+						'&pageSize=' + this.pageSize,
+					method: 'post',
+					data: {},
+					success: (res) => {
+						// console.log(res);
+						let data = res.data.data;
+						for (var i = 0; i < data.list.length; i++) {
+							this.houseList.push(data.list[i]);
+						}
 
-					}).then(res => {
+						if (data.list.length === 0) {
+							this.status = 'nomore';
+						} else {
+							this.status = 'loadmore';
+						}
+					}
+				});
+				/* 
+				this.$u.post(this.global_data.global_data.BaseUrl + 'property/search?cityPinYin=chengdu&pageNum=' + this.pageNum +
+					'&pageSize=' +
+					this.pageSize, {
+						cityPinYin: this.global_data.global_data.city.cityPinYin,
+						pageNum: this.pageNum,
+						pageSize: this.pageSize,
+						searchParam: {
+							areaId: 0,
+							areaLevel: 0,
+							completeYearRanges: [{
+								maxValue: 100,
+								minValue: 0
+							}],
+							countFRanges: [{
+								maxValue: 100,
+								minValue: 0
+							}],
+							decorations: [
+								"精装"
+							],
+							directions: [
+								"string"
+							],
+							floors: [
+								"string"
+							],
+							houseAreaRanges: [{
+								maxValue: 100,
+								minValue: 0
+							}],
+							houseTypes: [
+								"string"
+							],
+							keyWords: "string",
+							rentRanges: [{
+								maxValue: 100,
+								minValue: 0
+							}],
+							saleRanges: [{
+								maxValue: 100,
+								minValue: 0
+							}],
+							sortByHouseArea: -1,
+							sortByLatest: -1,
+							sortBySoldPrice: -1,
+							sortByUnitPrice: -1
+						}
+					}, {}).then(res => {
 					//console.log(res);
-					for(var i = 0; i < res.data.list.length; i++){
+					for (var i = 0; i < res.data.list.length; i++) {
 						this.houseList.push(res.data.list[i]);
 					}
-					
-					if(res.data.list.length === 0){
+
+					if (res.data.list.length === 0) {
 						this.status = 'nomore';
-					}
-					else{
+					} else {
 						this.status = 'loadmore';
 					}
 				})
+			 */
 			},
 
 			loadmore() {
 				this.pageNum = this.pageNum + 1;
 				this.GetHouseList();
 			},
+
+			getAreaList() {
+				this.$u.get("https://gl.junjunhouse.com/area/getAreaByPCode", {
+					cityPinYin: this.global_data.global_data.city.cityPinYin,
+					pAreaCode: 1
+				}).then(res => {
+					let data = res.data;
+					if (res.code == 200) {
+						if (!data) return;
+						for (var i = 0; i < res.data.length; i++) {
+							this.areaList.push({
+								value: i + 1,
+								label: res.data[i].areName
+							});
+						}
+					}
+				})
+			}
 		},
 
 		onPageScroll(e) {
